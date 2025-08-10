3ab86355d465d20e0cee6a2f172d3ec874f9a209
fatih3457 <fatihozcan90@outlook.com>
2025-08-11 01:14:00 +0300

    Inıtial commit

---
+++ src/main/java/com/iskiradar/iskiradarapi/IskiRadarApiApplication.java
@@ -0,0 +1,13 @@
+package com.iskiradar.iskiradarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IskiRadarApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IskiRadarApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iskiradar/iskiradarapi/IskiRadarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iskiradar.iskiradarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IskiRadarApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
