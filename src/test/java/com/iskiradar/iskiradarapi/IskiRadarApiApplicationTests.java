196db35a0ba385781957ccb3c50a226d5d8426d3
fatih3457 <fatihozcan90@outlook.com>
2025-08-19 01:54:49 +0300

    some updates

--- src/test/java/com/iskiradar/iskiradarapi/IskiRadarApiApplicationTests.java
+++ src/test/java/com/iskiradar/iskiradarapi/IskiRadarApiApplicationTests.java
@@ -3,6 +3,7 @@ package com.iskiradar.iskiradarapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class IskiRadarApiApplicationTests {
 
