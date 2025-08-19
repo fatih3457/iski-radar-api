2b96220ec09faf8d00bda32236a4a50140f5b78e
fatih3457 <fatihozcan90@outlook.com>
2025-08-20 01:37:56 +0300

    feat: Add /graph endpoint for historical dam data

--- src/main/java/com/iskiradar/iskiradarapi/IskiRadarApiApplication.java
+++ src/main/java/com/iskiradar/iskiradarapi/IskiRadarApiApplication.java
@@ -2,8 +2,10 @@ package com.iskiradar.iskiradarapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class IskiRadarApiApplication {
 
     public static void main(String[] args) {
